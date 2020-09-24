5f467a56d6aa1cb808e242f713ff6836bd241b76
Denvin <39243323+Denvin@users.noreply.github.com>
2020-09-24 20:22:12 +0300

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello, I'm " + name);
+    }
+
+    // Update is called once per frame
+    public void Attack(int damageParam)
+    {
+        health -= damageParam;
+        print("health left: " + health);
+        if (health <= 0) 
+        {
+            print(name + " I'm dead");
+        }
+    }
+    public int GetHealth()
+    {
+        return health;
+    }
+    void Update()
+    {
+        
+    }
+} 
---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int health;
+        int enemy;
+
+        health = 100;
+        enemy = health;
+        print(enemy);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
