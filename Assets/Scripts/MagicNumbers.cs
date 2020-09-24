37fb03fab2d0efa82eb00df2cadc9398ba6bc949
Denvin <39243323+Denvin@users.noreply.github.com>
2020-09-24 21:06:34 +0300

    Занятие 3

--- Assets/Scripts/MagicNumbers.cs
+++ Assets/Scripts/MagicNumbers.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MagicNumbers : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+    public Text title;
     
     void Start()
     {
-        
+        title.text = "Magic Numbers";
+        title.color = Color.cyan;
     }
 
     // Update is called once per frame
